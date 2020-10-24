7d7adde6c1eab3dcca9e2516ae9b872fe5edf41a
Victoria Lee <victoriacmlee@gmail.com>
2020-10-24 19:33:03 -0400

    pesudo code update

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,34 @@
+// required packages
+const fs = require ("fs");
+const inquirer = require("inquirer");
+const util = require("until");
+const open = require("open");
+const generateMarkdown = require("./utils/generateMarkdown")
+
+// turn the writefile into a promise structure
+const writeAsync = util.promisify(fs.writeFile)
+
 // array of questions for user
 const questions = [
-
+    // list of questions 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    // write file with fileName and date
+
+    // if there is error
+
 }
 
 // function to initialize program
-function init() {
+async function init() {
+
+    // save response to variable
+   
+    // generate markdown to format the data set up as html page
+
+    // call writeToFile function passing answer data
 
 }
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,15 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
+  // return the data 
+  return `# ${data.title}`;
+
+  // console.log to see the data
+
+  // format custom data to display on page to insert the data with template literals
+
+  // return the data
+  
 }
 
 module.exports = generateMarkdown;
